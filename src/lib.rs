bcc138e0a8ddf2574836244de7c2a42c156ed54a
Dan Herrera <whoahbot@bytewax.io>
2023-03-17 10:50:34 -0700

    Add rdkafka

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,65 @@
-
 use pyo3::prelude::*;
 
+use rdkafka::{TopicPartitionList, ClientContext};
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::consumer::{ConsumerContext, Rebalance, Consumer};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::error::KafkaResult;
+
+// A context can be used to change the behavior of producers and consumers by adding callbacks
+// that will be executed by librdkafka.
+// This particular context sets up custom callbacks to log rebalancing events.
+struct CustomContext;
+
+impl ClientContext for CustomContext {}
+
+impl ConsumerContext for CustomContext {
+    fn pre_rebalance(&self, rebalance: &Rebalance) {
+        println!("Pre rebalance {:?}", rebalance);
+    }
+
+    fn post_rebalance(&self, rebalance: &Rebalance) {
+        println!("Post rebalance {:?}", rebalance);
+    }
+
+    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
+        println!("Committing offsets: {:?}", result);
+    }
+}
+
+
+// A type alias with your custom consumer can be created for convenience.
+type LoggingConsumer = StreamConsumer<CustomContext>;
+
+#[no_mangle]
+fn start_consumer() {
+    let context = CustomContext;
+    
+    let consumer: LoggingConsumer = ClientConfig::new()
+        .set("group.id", "buh")
+        .set("bootstrap.servers", "localhost:9092")
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        //.set("statistics.interval.ms", "30000")
+        //.set("auto.offset.reset", "smallest")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create_with_context(context)
+        .expect("Consumer creation failed");
+
+    consumer
+        .subscribe(&["buh"])
+        .expect("Can't subscribe to specified topics");
+}
+
 /// Formats the sum of two numbers as string.
+#[pyfunction]
+fn a_consumer() -> PyResult<()> {
+    start_consumer();
+    Ok(())
+}
+
+/// dummy fn to create consumer
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
@@ -11,5 +69,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn ohio(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(a_consumer, m)?)?;    
     Ok(())
 }
